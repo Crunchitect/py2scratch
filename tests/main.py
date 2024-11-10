d38fb293d7ea32cb4491946e8229c4254c9dac46
Crunchitect <95293374+Crunchitect@users.noreply.github.com>
2024-11-10 16:32:00 +0700

    Make library installable

--- tests/main.py
+++ tests/main.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.insert(0, '..')
-from src.scratch import Project, Stage, Sprite, Sound, Costume, RotationStyles
+from py2scratch.scratch import Project, Stage, Sprite, Sound, Costume, RotationStyles
 
 proj = Project()
 
